9f56733456a142e123e45ffe69cb1e94d84c97f4
Will McNeilly <wmcneilly@rumblelabs.com>
2012-10-21 14:02:55 +0100

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DontBreakTheChain::Application.config.secret_token = 'e833e1ea17de097b1335a457eb19ce1861a8a47eab43d5af8bc70da4e477d491b9043e75f47d83d8b640697d0a9111bd4458aac9ae558a083302754ed0c65717'
